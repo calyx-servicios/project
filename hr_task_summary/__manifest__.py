aca7d7e099dbfa930a615fd03f97e22c0628b049
paradisocrist <crisparadiso_@hotmail.com>
2020-11-20 07:24:41 -0300

    [add][hr_task_summary]odoo-285

--- hr_task_summary/__manifest__.py
+++ hr_task_summary/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Task Summary',
+    'name': 'Task & Account Summary',
     'author': 'Calyx Servicios S.A., Odoo Community Association (OCA)',
     'website': 'http://odoo.calyx-cloud.com.ar/',
     'license': 'AGPL-3',
@@ -11,6 +11,7 @@
     'installable': True,
     'depends': ['hr', 'project','hr_timesheet'],
     'data': [
-        'views/hr_views.xml',
+        'views/project_views.xml',
+        'views/project_line_views.xml',
      ],
 }
